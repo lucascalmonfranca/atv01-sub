2ffe7ff51361da95137a28c1e771445bfafefa39
Pinto Francês <lucas.calmon.franca@gmail.com>
2026-05-25 10:19:44 -0300

    Add files via upload

---
+++ atv01sub/atv01sub/src/script.js
@@ -0,0 +1,11 @@
+function abrirTwitter(){
+    window.location.href = "https://twitter.com";
+}
+
+function abrirInstagram(){
+    window.location.href = "https://instagram.com";
+}
+
+function abrirYoutube(){
+    window.location.href = "https://youtube.com";
+}
